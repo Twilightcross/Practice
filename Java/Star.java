dff86b025fa02426cf27619a33bb522ec100443f
Park Dae Geun <48747290+Twilightcross@users.noreply.github.com>
2019-04-02 23:35:14 +0900

    Update Star.java

--- Java/Star.java
+++ Java/Star.java
@@ -13,7 +13,7 @@ class Star
 			System.out.println();
 		}
 	}
-} // 화면에 별 찍기 
+} // 내림차순 삼각형 (왼쪽)
 
 
 		for (int i=1; i<10 ; i++)
@@ -24,4 +24,44 @@ class Star
 			System.out.println();
 		}
 	}
-} // 위 내용을 반대로 
+} // 오름차순 삼각형(왼쪽)
+
+for (int a=0; a<10 ; a++)
+	{
+		for (int b=0; b<=10; b++)
+		{
+			System.out.print(b<(10-a)? " " : "*");
+		}
+		System.out.println();
+	}
+	}
+} //내림차순 삼각형 (오른쪽)
+
+for (int a=0; a<10 ;a++ )
+		{
+			for (int b=0; b<10; b++)
+			{
+				System.out.print(b<a? " " : "*");
+			}
+			System.out.println();
+		}
+	}
+} //내림차순 삼각형 (오른쪽)
+
+for (int a=0; a<4 ;a++ )
+		{
+			for (int b=0; b<3-a; b++)
+			{
+				System.out.print(" "); //띄어쓰기 꼭!
+			}
+			for (int b=0;b<2*a+1 ;b++ )
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+} //피라미드 모양 별 찍기 // 아직 이해 어려움 
+
+
+
