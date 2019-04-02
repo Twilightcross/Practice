30d367bdac4f5e81277e6829aed43505b3727c86
Park Dae Geun <48747290+Twilightcross@users.noreply.github.com>
2019-04-03 00:05:34 +0900

    Update Star.java

--- Java/Star.java
+++ Java/Star.java
@@ -84,4 +84,21 @@ for (int i=0; i<5 ; i++ )
 	}
 } //정삼각 별 찍기 2
 
+for(int i =1; i<5; i++)
+{
+		for(int j=1;j<5-i;j++)
+
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=2*i-1;j++)
+			{
+			System.out.print("*");
+			}
+			System.out.println("");
+}
+}
+
+} //정삼각 별 찍기 3 (가장 경제적이라고 여겨짐 ) 
+
 
