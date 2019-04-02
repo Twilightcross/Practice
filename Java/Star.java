e230029740211678d00530a1210598ce6d061c76
Park Dae Geun <48747290+Twilightcross@users.noreply.github.com>
2019-04-02 18:12:08 +0900

    Update Star.java

--- Java/Star.java
+++ Java/Star.java
@@ -13,7 +13,7 @@ class Star
 			System.out.println();
 		}
 	}
-}
+} // 화면에 별 찍기 
 
 
 		for (int i=1; i<10 ; i++)
@@ -24,4 +24,4 @@ class Star
 			System.out.println();
 		}
 	}
-}
+} // 위 내용을 반대로 
