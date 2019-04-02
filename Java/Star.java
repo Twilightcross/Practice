82d7633af1ae4d6827a3d5549d7647733df848e8
Park Dae Geun <48747290+Twilightcross@users.noreply.github.com>
2019-04-02 18:11:40 +0900

    Create Star.java

---
+++ Java/Star.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Star
+{
+	public static void main(String[] args) 
+	{	
+
+		for (int i=1; i<11 ; i++)
+			{ 
+			for (int j=10; j>i ; j-- ){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+		for (int i=1; i<10 ; i++)
+			{ 
+			for (int j=0; j<i ; j++ ){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
